b32304a8b4251b323b98343734eb0121d826fb09
Giuseppe Habib <122796518+GlobalDynamicsRG@users.noreply.github.com>
2025-02-25 13:57:05 +0100

    Delete old_files directory

--- old_files/Unforced_Duffing_Oscillator/sistema.m
+++
@@ -1,9 +0,0 @@
-% double well (unforced)
-function xprime = sistema(t,x,par)
-%par = [zeta a b];
-    xprime=zeros(2,1);
-    % ============================= State Space ========================
-    xprime(1)=x(2);
-    xprime(2)=-2*par(1)*x(2)+par(2)*x(1)-par(3)*x(1)^3;
-    % ===================================================================
-end
--- old_files/functions/find_index.m
+++
@@ -1,9 +0,0 @@
-function index = find_index(cell,dim,discr)
-% find_index(cell,dim,discr)
-% from the coordinates of a cell, find its global index
-
-index = 1;
-
-for i = 1:dim
-    index = index+(cell(i)-1)*discr^(dim-i);
-end
